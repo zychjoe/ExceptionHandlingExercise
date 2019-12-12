6429685d9cd10e50d18d76ebdcaab9b23e3ed82c
nate <nrice@truecoders.io>
2019-12-11 18:14:10 -0600

    init commit

---
+++ ExceptionHandlingExercise/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ExceptionHandlingExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // First create an char[], it must contain 6 numbers and 3 letters
+            // Create a list called numbers
+            // Create an string variable with an empty string initializer
+
+            // using a foeach loop, attempt to parse the elements in your char[],
+            // Exceptions will be thrown 
+
+
+
+
+            // Make a foreach loop to iterate through your character array
+            
+                // Now create a try catch
+                
+                
+                    // Inside your try block
+                        // set your string variable to each array element in your char[] to .ToString()
+                        // Now, using int.Parse, parse your string variable and store in an int variable
+                        // Then add each int to your list
+                
+                // in your () of your catch, 
+                
+                    //Console.WriteLine($"Unable to Parse '{character}'");
+                
+            
+
+            //foreach (var num in numbers)
+            //{
+            //    Console.WriteLine(num);
+            //}
+        }
+    }
+}
